7f74c6f7aedc3690aaa92417556e3d1b4e158351
Abdullah Al Faroque <rfnshare@gmail.com>
2026-01-04 19:10:44 +0600

    fixed media issues

--- backend/building_manager/building_manager/urls.py
+++ backend/building_manager/building_manager/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path("api/complaints/", include("complaints.urls")),
     path("api/expenses/", include("expenses.urls")),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
