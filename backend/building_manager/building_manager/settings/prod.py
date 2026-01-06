fae24f0b8613515bb2b7e5b5cab43d97cda1afe2
Abdullah Al Faroque <rfnshare@gmail.com>
2026-01-06 20:15:57 +0600

    updated prod db settings

--- backend/building_manager/building_manager/settings/prod.py
+++ backend/building_manager/building_manager/settings/prod.py
@@ -11,13 +11,18 @@ ALLOWED_HOSTS = [
 # ------------------
 # Database (PROD)
 # ------------------
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+# else:
+# SQLite from base.py is used automatically
 
 # ------------------
 # Static files
