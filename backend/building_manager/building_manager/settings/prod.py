338002dabc12dada355d2ff013a337237cd477d4
Abdullah Al Faroque <rfnshare@gmail.com>
2026-01-07 01:48:00 +0600

    ff

--- backend/building_manager/building_manager/settings/prod.py
+++ backend/building_manager/building_manager/settings/prod.py
@@ -15,7 +15,7 @@ if DATABASE_URL:
         "default": dj_database_url.config(
             default=DATABASE_URL,
             conn_max_age=600,
-            ssl_require=True,
+            ssl_require=False,
         )
     }
 
