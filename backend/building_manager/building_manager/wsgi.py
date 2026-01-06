c212a09dfb51f2ef58ed01c11fd3bf747a493172
Abdullah Al Faroque <rfnshare@gmail.com>
2026-01-06 20:57:33 +0600

    fixed rooturl conf #2

--- backend/building_manager/building_manager/wsgi.py
+++ backend/building_manager/building_manager/wsgi.py
@@ -5,9 +5,11 @@ WSGI config for building_manager project.
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Do NOT hardcode dev/prod here
+# This will use DJANGO_SETTINGS_MODULE from environment (.env / PM2 / shell)
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "building_manager.settings.prod"
+    "building_manager.settings.base"
 )
 
 application = get_wsgi_application()
